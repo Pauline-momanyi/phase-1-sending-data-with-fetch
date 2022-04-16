dd76149d738000ef0a31241eddacf2296b110a8f
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-16 12:22:18 +0300

    Sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users',{
+        method: "POST",
+        headers:{
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(res => res.json())
+    .then(json => {
+        document.querySelector('body').innerHTML=json.id
+    })
+    .catch(error =>{
+        document.querySelector('body').innerHTML=error.message
+
+    })
+
+}
